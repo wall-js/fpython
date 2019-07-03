8dfe25ee7f9ba234e959014df328005de21d8496
Wall <307606056@qq.com>
2019-07-03 14:32:03 +0800

    版本更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fpython',
-    version='0.0.8',
+    version='0.0.13',
     keywords=('utils', 'packages'),
     long_description=open("README.rst").read(),
     description='packages',
